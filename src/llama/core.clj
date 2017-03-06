c80764f1213add6673ade9cc190ebdb63b5ca614
Antoine Kalmbach <antoine.kalmbach@qvantel.com>
2017-03-06 17:09:20 +0200

    heh

--- src/llama/core.clj
+++ src/llama/core.clj
@@ -1,7 +1,7 @@
 (ns llama.core
   "Core utilities for working with Camel endpoints, messages, exchanges, etc."
-  (:import org.apache.camel.impl.DefaultMessage
-           org.apache.camel.Message))
+  (:import [org.apache.camel Exchange Message]
+           [org.apache.camel.impl DefaultCamelContext DefaultMessage]))
 
 (defn string->message
   "Create a Message, with `body` String as the body. Optionally with
@@ -22,6 +22,11 @@
     (string->message (str body) :id id :headers headers)
     body))
 
+(defn out-capable?
+  "If the Exchange in `x` is an `InOut` exchange or not."
+  [^Exchange x]
+  (.. x (getPattern) (isOutCapable)))
+
 (defn reply
   "Reply successfully to an exchange with `body`. `body` can be a String message
   or a [Message](http://camel.apache.org/message.html). If `body` is string it is
@@ -41,7 +46,7 @@
   "
   [exchange body & {:keys [headers id] :or {headers (hash-map)
                                             id nil}}]
-  (when (.isOutCapable (.getPattern exchange))
+  (when (out-capable? exchange)
     (let [out (.getOut exchange)
           m (ensure-message body id headers)]
       (.setOut exchange m))))
@@ -64,15 +69,47 @@
 
 ```
 (route (from \"direct:foo\")
-       (process (fn [x] (reply x \"hello\"))))
+       (process (fn [x] (reply x \"Is it African or European?\"))))
   
 ;; start the route, add to context, etc.
 
-(request-body ctx \"direct:foo\" \"hi\")
-;; => \"hello\"
+(request-body ctx \"direct:foo\" \"How much weight can an unladen swallow carry?\")
+;; => \"Is it African or European?\"
 ```
 "
   [ctx endpoint body & {:keys [headers]
                         :or {headers {}}}]
   (let [producer (.createProducerTemplate ctx)]
     (.requestBodyAndHeaders producer endpoint body headers)))
+
+(defn x-in
+  "Get the `In` part of an `InOnly` or `InOut` exchange."
+  [^Exchange x]
+  (.getIn x))
+
+(defn x-out
+  "Get the `Out` part of an `InOut` exchange. Returns nil if not `InOut`."
+  [^Exchange x]
+  (when (out-capable? x)
+    (.getOut x)))
+
+(defn body
+  "Get the body of the message `msg`. 
+
+  If `clazz` is provided use a [type converter](http://camel.apache.org/type-converter.html) to cast it to `clazz`. Throws
+[`NoTypeConversionAvailableException`](http://camel.apache.org/maven/current/camel-core/apidocs/org/apache/camel/NoTypeConversionAvailableException.html)
+if conversion fails because the type converter isn't in the registry. Throws `TypeConversionException` if the conversion itself fails.
+
+  Uses the context of the exchange of msg. If `msg` has no exchange and context then the `DefaultCamelcontext` is used."
+  ([^Message msg] (.getBody body))
+  ([^Message msg
+    ^java.lang.Class clazz]
+   (let [ctx (if-some [ctx (.getContext (.getExchange msg))]
+               (ctx)
+               (DefaultCamelContext.))
+         body (.getBody msg)]
+     (.mandatoryConvertTo (.getConverter ctx) body clazz))))
+
+(defn set-body
+  [^Message msg body]
+  (.setBody msg body))
